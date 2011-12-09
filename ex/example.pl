f27793f954e84c9432df14ebefdb62cbe4cfa5e2
Joel Berger <joel.a.berger@gmail.com>
2011-12-09 11:13:11 -0600

    Updated example.

--- ex/example.pl
+++ ex/example.pl
@@ -4,8 +4,9 @@ use lib 'lib';
 use Moose;
 use MooseX::Types::NumUnit qw/num_of_unit/;
 
-#$MooseX::Types::NumUnit::Verbose = 1;
+$MooseX::Types::NumUnit::Verbose = 1;
 
+has 'length' => ( isa => num_of_unit( 'm' ), is => 'rw', default => '1 ft' );
 has 'speed' => ( isa => num_of_unit('ft / hour'), is => 'rw', required => 1 );
 
 no Moose;
@@ -14,4 +15,5 @@ __PACKAGE__->meta->make_immutable;
 my $test = MyTest->new( speed => '2 m / s' );
 
 print $test->speed, "\n";
+print $test->length, "\n";
 
